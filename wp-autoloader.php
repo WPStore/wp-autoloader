0544d65e3f70b9983930e165c3394a2aeb622931
Christian Foellmann <foellmann@foe-services.de>
2017-09-08 16:08:30 +0200

    Corrected license and proper composer.json

--- wp-autoloader.php
+++ wp-autoloader.php
@@ -2,11 +2,36 @@
 /**
  * @author    WPStore.io <code@wpstore.io>
  * @copyright Copyright (c) 2017, WPStore.io
- * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0
- * @package   WPStore\WPUtils\Autoloader
+ * @license   https://spdx.org/licenses/MIT.html MIT
+ * @package   WPStore\WPStoreUtils\Autoloader
  * @version   0.1.0
  */
-namespace WPUtils;
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 WPStore.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace WPStoreUtils;
 
 /**
  * An WPUtilities of a general-purpose implementation that includes the optional
@@ -31,7 +56,7 @@ namespace WPUtils;
  *
  *      <?php
  *      // instantiate the loader
- *      $loader = new \WPUtils\Autoloader;
+ *      $loader = new \WPStoreUtils\Autoloader;
  *
  *      // register the autoloader
  *      $loader->register();
@@ -51,6 +76,12 @@ namespace WPUtils;
  *
  *      <?php
  *      new \Foo\Bar\Qux\QuuxTest;
+ */
+
+/**
+ * Class Autoloader
+ * @package WPStoreUtils
+ * @See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
  */
 class Autoloader {
 	/**
